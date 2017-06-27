cedf16523cbd3d6d96624a18d224cc42ca324cb6
Michael Lu <mlu@e1z2r10p1.42.us.org>
2017-06-27 14:12:59 -0700

    Swapping to less brute force, more simpler validation

--- shape_validate.c
+++ shape_validate.c
@@ -22,95 +22,138 @@
 /*   #...   ##..   #...   ....   ....   #...   .#..   ##..   ....  */
 /*   ....   ....   #...   ....   ....   ....   ....   ....   ....  */
 
-int g_ct;
 int g_size;
+int g_square;
 
-int	ft_vShape2(char *str, int fill)
+int	ft_scan_ws(char *str, int i, int count)
 {
-	if (fill == 3 && str[g_ct] == '#' && str[g_ct - 5] == '#')
-		return (2);
-	else if (fill == 3 && str[g_ct] == '#' && str[g_ct - 4] == '#')
-		return (3);
-	else if (fill == 3 && str[g_ct] == '#' && str[g_ct - 3] == '#')
-		return (4);
-	else if (fill == 2 && str[g_ct] == '#' && str[g_ct + 1] == '#')
-		return (5);
-	else if (fill == 2 && str[g_ct] == '#' && str[g_ct - 5] == '#' && str[g_ct +5] == '#')
-		return (6);
-	else if (fill == 2 && str[g_ct + 1] == '#' && str[g_ct - 5] == '#' && str[g_ct + 5] == '#')
-		return (8);
-	else if (fill == 2 && str[g_ct + 1] == '#' && str[g_ct + 2] == '#'
-		&& str[g_ct - 5] == '#' && str[g_ct - 4] == '#')
-		return (7);
-	else if (fill == 2 && str[g_ct] == '#' && str[g_ct + 1] == '#'
-		&& str[g_ct - 4] == '#' && str[g_ct - 3] == '#')
-		return (14);
-	else if (fill == 1)
-		return (ft_vShape3(*str, fill));
-	else if(str[g_ct] == '\n')
-		return (0);
-	else
+	if (count == 4)
+		return (1);
+	if (str[i + 1] == '#')
 	{
-		g_ct++;
-		return (ft_vShape2(*str, fill));
+		count++;
+		i++;
+		return (ft_scan_ws(*str, i, count));
+	}
+	else if (str[i + 5] == '#')
+	{
+		count++;
+		i = i + 5;
+		return (ft_scan_wes(*str, i, count));
+	}
+	else
+		return (0);
+}
+
+int	ft_scan_wes(char *str, int i, int count)
+{
+	if (count == 4)
+		return (1);
+	if (str[i + 1] == '#')
+	{
+		count++;
+		i++;
+		return (ft_scan_es(*str, i, count));
+	}
+	else if (str[i + 5] == '#')
+	{
+		count++;
+		i = i + 5;
+		return (ft_scan_wes(*str, i, count));
+	}
+	else if (str[i - 1] == '#')
+	{
+		count++;
+		i = i + 5;
+		return (ft_scan_ws(*str, i, count));
+	}
+	else
+		return (0);
+}
+
+int	ft_scan_es(char *str, int i, int count)
+{
+	if (count == 4)
+		return (1);
+	if (str[i + 1] == '#')
+	{
+		count++;
+		i++;
+		return (ft_scan_es(*str, i, count));
+	}
+	else if (str[i + 5] == '#')
+	{
+		count++;
+		i = i + 5;
+		return (ft_scan_wes(*str, i, count));
+	}
+	else
+		return (0);
+}
+
+int	ft_scan(char *str)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '#')
+		i++;
+	count++;
+	if (str[i + 1] == '#')
+	{
+		count++;
+		i++;
+		return (ft_scan_es(*str, i, count));
+	}
+	else if (str[i + 5] == '#')
+	{
+		count++;
+		i = i + 5;
+		return (ft_scan_wes(*str, i, count));
+	}
+	else
+		return (0);
+}
+
+int	ft_isvalid(char *str)
+{
+	int i;
+	int count;
+
+	i = g_size - 1;
+	count = 0;
+	while (++i < (g_size + 21) && str[i] != '\0')
+	{
+		if (((i + 1) % 5 == 0) && str[i] != '\n')
+			return (0);
+		else if (((i + 1) % 5 != 0) && (str[i] != '#' || str[i] != '.'))
+			return (0);
+		if (str[i] == '#')
+			count++;
+	}
+	if (count != 4)
+		return (0);
+	if (str[i] != '\0')
+	{
+		g_size = g_size + 21;
+		return (ft_isvalid(*str));
+	}
+	else if (str[i] == '\0')
+	{
+		g_square = (g_size / 21);
+		return (ft_scan(*str));
 	}
 }
 
-int	ft_vShape1(char *str)
-{
-	int fill;
-
-	fill = 0;
-	g_ct++;
-
-	while (str[g_ct] != '\0')
-	{
-		if (str[g_ct++] == '#')
-			fill++;
-		else if (str[g_ct] == '\n')
-		{
-			if (fill == 4)
-				return (1);
-			else if (fill != 0 && ft_isValid)
-				return (ft_vsShape2(*str, fill));
-			else if (!ft_isValid && fill != 0)
-				return (0);
-			else
-				return (ft_vShape1(*str));
-		}
-		else
-			g_ct++;
-	}
-	return (0);
-}
-
-int	ft_isValid(char *str, int fill)
-{
-	g_ct++;
-
-	if (fill == 3 && str[g_ct - 5] == '#' && str[g_ct - 4] == '#' && str[g_ct - 3] == '#' 
-		|| fill == 3 && str[g_ct - 4] == '#' && str[g_ct - 3] == '#' && str[g_ct - 2] == '#')
-		return (1);
-	else if (fill == 3)
-		return (0);
-	else if (fill == 2 && str[g_ct - 5] == '#' && str[g_ct - 4] == '#'
-		|| fill == 2 && str[g_ct - 4] == '#' && str[g_ct - 3] == '#'
-		|| fill == 2 && str[g_ct - 3] == '#' && str[g_ct - 2] == '#')
-		return (1);
-	else if (fill == 2)
-		return (0);
-	else
-		return (1);
-}
-
-int ft_vShape(char *str)
+int ft_validate(char *str)
 {
 	int shape;
 
-	g_ct = -1;
 	shape = 0;
-
-	shape = ft_vshape_row1(*str);
+	g_size = 0;
+	shape = ft_isvalid(*str);
 	if (shape != 0)
 	{
 		g_size++;
